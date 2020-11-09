5fde538daec89e43fc898e25d58337736cff49ea
Alexander Barnett <agraybarnett@gmail.com>
2020-11-09 19:03:57 +0000

    added last_epoch to lr_scheduler to enable resuming experiments

--- train.py
+++ train.py
@@ -69,7 +69,7 @@ def set_up_experiment(params, experiment, resume=None):
         params["device"]) is int and torch.cuda.is_available() else "cpu")
 
     model, optimizer_state_dict = load_model(params, resume)
-
+    
     # Create experiment directory
     if resume:
         experiment_dir = os.path.split(resume)[0]  # Use existing folder
@@ -131,7 +131,7 @@ def set_up_experiment(params, experiment, resume=None):
     utils.optimizer_to_gpu(optimizer)
 
     # LR Scheduler
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=params["lr_epoch_step_size"], gamma=0.1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=params["lr_epoch_step_size"], gamma=0.1, last_epoch=params["start_epoch"])
     
     return params, train_loader, val_loader, test_loader, model, criterion, optimizer, scheduler
 
