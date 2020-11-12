b0488b484155e3c5c9bc5e0937b039fa0153e762
Alex Barnett <agraybarnett@gmail.com>
2020-11-12 10:24:26 -0600

    made torchscript conversion script better

--- convert_torchscript.py
+++ convert_torchscript.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 import argparse
@@ -6,24 +7,35 @@ import models
 
 parser = argparse.ArgumentParser(description='FastDepth evaluation')
 parser.add_argument('-m', '--model', type=str, required=True, help="Path to model.")
+parser.add_argument('--save-gpu', action='store_true')
 args = parser.parse_args()
 
 model_path = args.model
-
 
 model_state_dict, _, _, _ = utils.load_checkpoint(args.model)
 model_state_dict = utils.convert_state_dict_from_gpu(model_state_dict)
 model = models.MobileNetSkipAdd(output_size=(224, 224), pretrained=True)
 if model_state_dict:
     model.load_state_dict(model_state_dict)
-model.to(torch.device("cuda:0"))
 
-# An example input you would normally provide to your model's forward() method.
-example = torch.rand(1, 3, 224, 224).cuda()
+if args.save_gpu:
+    print("Saving model on GPU")
+    model.to(torch.device("cuda:0"))
+    example = torch.rand(1, 3, 224, 224).cuda()
+else:
+    print("Saving model on CPU")
+    model.to(torch.device("cpu"))
+    example = torch.rand(1, 3, 224, 224)
+
+model.eval()
 
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
 traced_script_module = torch.jit.trace(model, example)
 
-save_path = "./results/fastdepth_1.0.0/traced_fastdepth_1.0.0_model.pt"
+model_dir = model_path.split('/')[:-1][0]
+model_name = model_path.split('/')[-1]
+device_ext = "gpu" if args.save_gpu else "cpu"
+
+save_path = os.path.join(model_dir, 'traced_' + model_name[:-4] + "_" + device_ext + ".pt")
 traced_script_module.save(save_path)
 print("Saved to ", save_path)
